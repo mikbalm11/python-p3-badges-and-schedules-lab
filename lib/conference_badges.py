d4b3ff14241015899c5531ad94e8404b1103d43a
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-09-17 23:52:18 -0400

    passes all tests!

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,12 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!" for name in names]
 
 def printer(names):
-    return None
+    [print(badge) for badge in batch_badge_creator(names)]
+    [print(assignment) for assignment in assign_rooms(names)]
